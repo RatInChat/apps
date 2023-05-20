fb2b6c50a6c0f05d3e7d75c68c331bd7e8a4c13f
RatInChat <vishyfishyalt@gmail.com>
2023-05-20 15:17:06 -0500

    Update settings.js

--- apps/settings.js
+++ apps/settings.js
@@ -1,57 +1,66 @@
 module.exports = {
-    logo: 'https://static.thenounproject.com/png/1008927-200.png',
-    ads: false,
-    async execute(mainframe, data, page) {
-        // make the page on mainframe
-        const box = document.createElement('div');
-        box.id = 'settingsdd1683592387221_1683592387222_32830ufdskjhafdisa8y839yhfidso';
-        box.style.width = '100vw';
-        box.style.height = `calc(100vh - 55px)`;
-        box.style.top = '0';
-        box.style.position = 'fixed';
-        box.style.backgroundColor = '#101010';
-        box.style.borderRadius = '5px';
-        box.style.zIndex = '10000';
-        box.style.display = 'flex';
-        box.style.flexDirection = 'column';
-        box.style.alignItems = 'center';
-        box.style.justifyContent = 'center';        
-        mainframe.appendChild(box);
-        
-        const title = document.createElement('h1');
-        title.innerText = page;
-        box.appendChild(title);
-        const close = document.createElement('button');
-        close.innerText = 'X';
-        close.style = `
-            position: absolute;
-            top: 0;
-            right: 0;
-            background-color: #101010;
-            color: #ffffff;
-            border: none;
-            border-radius: 5px;
-            padding: 5px;
-            margin: 5px;
-            cursor: pointer;
-        `
+  logo: 'https://static.thenounproject.com/png/1008927-200.png',
+  ads: false,
+  async execute(mainframe, data, page) {
+    // Create the settings container
+    const box = document.createElement('div');
+    box.id = 'settingsdd1683592387221_1683592387222_32830ufdskjhafdisa8y839yhfidso';
+    box.style.width = '100vw';
+    box.style.height = `calc(100vh - 55px)`;
+    box.style.top = '0';
+    box.style.position = 'fixed';
+    box.style.backgroundColor = '#F8F8F8';
+    box.style.borderRadius = '5px';
+    box.style.zIndex = '10000';
+    box.style.display = 'flex';
+    box.style.flexDirection = 'column';
+    box.style.alignItems = 'center';
+    box.style.justifyContent = 'center';        
+    mainframe.appendChild(box);
 
-        close.addEventListener('mouseover', () => {
-            close.style.backgroundColor = 'red';
-        });
+    // Create the title
+    const title = document.createElement('h1');
+    title.innerText = page;
+    box.appendChild(title);
 
-        close.addEventListener('mouseout', () => {
-            close.style.backgroundColor = '#101010';
-        });
+    // Create the close button
+    const close = document.createElement('button');
+    close.innerText = 'X';
+    close.style = `
+      position: absolute;
+      top: 0;
+      right: 0;
+      background-color: #F8F8F8;
+      color: #000000;
+      border: none;
+      border-radius: 5px;
+      padding: 5px;
+      margin: 5px;
+      cursor: pointer;
+    `;
 
-        close.addEventListener('click', () => {
-            box.remove();
-        });
+    close.addEventListener('mouseover', () => {
+      close.style.backgroundColor = 'red';
+      close.style.color = 'white';
+    });
 
-        box.appendChild(close);
+    close.addEventListener('mouseout', () => {
+      close.style.backgroundColor = '#F8F8F8';
+      close.style.color = '#000000';
+    });
 
-        if (page == 'Preferences') {
-            console.log('I think it worked')
-        }
+    close.addEventListener('click', () => {
+      box.remove();
+    });
+
+    box.appendChild(close);
+
+    if (page == 'Preferences') {
+      // Add preferences content
+      const content = document.createElement('div');
+      content.style.marginTop = '20px';
+      content.innerText = 'Preferences content goes here.';
+      box.appendChild(content);
     }
-}
+  }
+};
