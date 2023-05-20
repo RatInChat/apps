434b0be840668970f3b3249b32e61ad46b538fa7
RatInChat <vishyfishyalt@gmail.com>
2023-05-20 13:05:18 -0500

    Update settings.js

--- apps/settings.js
+++ apps/settings.js
@@ -5,14 +5,22 @@ module.exports = {
         // make the page on mainframe
         const box = document.createElement('div');
         box.id = 'settingsdd1683592387221_1683592387222_32830ufdskjhafdisa8y839yhfidso';
-        box.style.width = '100%';
-        box.style.height = '100%';
-        box.style.backgroundColor = 'white';
-        box.style.color = 'black';
+        
+        box.style.width = 'calc(100% - 10px)';
+        box.style.height = 'calc(100% - 10px)';
+        box.style.position = 'absolute';
+        box.style.top = '5px';
+        box.style.left = '5px';
+        box.style.backgroundColor = 'rgba(16, 16, 16, 0.9)';
+        box.style.borderRadius = '5px';
+        box.style.zIndex = '10000';
         box.style.display = 'flex';
         box.style.flexDirection = 'column';
         box.style.alignItems = 'center';
         box.style.justifyContent = 'center';
+        box.style.border = '1px solid #000000';
+        box.style.boxShadow = '0px 0px 10px 0px #000000';
+        
         mainframe.appendChild(box);
         
         const title = document.createElement('h1');
